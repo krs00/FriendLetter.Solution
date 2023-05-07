3164fec7ce62fb7b578a3467f476b0b4e27d2bbf
Kymani Stephens <kymanist00@gmail.com>
2023-05-07 15:33:39 -0700

    add route decorator

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -2,8 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FriendLetter.Controllers
 {
-  public class HomeController : Controller
-  {
+    public class HomeController : Controller
+    {
+          [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
 
-  }
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")] 
+    public string Letter() { return "Our virtual postcard will go here soon!"; }
+    }
 }
