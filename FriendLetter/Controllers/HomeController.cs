f1cbb9c6ae4c0e11eaf714cb517761bd3ca1aa67
Kymani Stephens <kymanist00@gmail.com>
2023-05-07 16:24:49 -0700

    add files

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -3,26 +3,35 @@ using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    [Route("/hello")]
-    public string Hello() { return "Hello friend!"; }
-
-    [Route("/goodbye")]
-    public string Goodbye() { return "Goodbye friend."; }
-
-    [Route("/")]
-    public ActionResult Letter()
+    public class HomeController : Controller
     {
-      LetterVariable myLetterVariable = new LetterVariable();
-      myLetterVariable.Recipient = "Lina";
-      myLetterVariable.Sender = "Jasmine";
-      return View(myLetterVariable);
+
+        [Route("/hello")]
+        public string Hello() { return "Hello friend!"; }
+
+        [Route("/goodbye")]
+        public string Goodbye() { return "Goodbye friend."; }
+
+        [Route("/")]
+        public ActionResult Letter()
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.Recipient = "Lina";
+            myLetterVariable.Sender = "Jasmine";
+            return View(myLetterVariable);
+        }
+
+        [Route("/form")]
+        public ActionResult Form() { return View(); }
+
+        [Route("/postcard")]
+        public ActionResult Postcard(string recipient, string sender)
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.Recipient = recipient;
+            myLetterVariable.Sender = sender;
+            return View(myLetterVariable);
+        }
+
     }
-
-    [Route("/form")]
-    public ActionResult Form() { return View(); }
-
-  }
 }
